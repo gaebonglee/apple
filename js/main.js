e7aec56d9785d4a32e8ab1be58a585dfb9444d0b
gayoung lee <ken604@naver.com>
2024-12-18 19:10:52 +0900

    장바구니 드롭다운

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const basketStarterEl = document.querySelector("header .basket-starter");
+const basketEl = document.querySelector("header .basket");
+
+basketStarterEl.addEventListener("click", function (event) {
+  event.stopPropagation();
+  if (basketEl.classList.contains("show")) {
+    //hide
+    hideBasket();
+  } else {
+    //show
+    showBasket();
+  }
+});
+basketEl.addEventListener("click", function (event) {
+  event.stopPropagation();
+});
+
+window.addEventListener("click", function () {
+  hideBasket();
+});
+
+function showBasket() {
+  basketEl.classList.add("show");
+}
+function hideBasket() {
+  basketEl.classList.remove("show");
+}
