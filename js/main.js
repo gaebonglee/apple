d970ff3eaf751970bbe809264ae6aa0b662dfd92
gayoung lee <ken604@naver.com>
2024-12-20 02:42:31 +0900

    css, javascript 활용하여 헤더애니메이션 완료

--- js/main.js
+++ js/main.js
@@ -29,11 +29,12 @@ function hideBasket() {
 
 // 검색!!
 const headerEl = document.querySelector("header");
-const hedaerMenuEls = [...headerEl.querySelectorAll("ul.menu > li")];
+const headerMenuEls = [...headerEl.querySelectorAll("ul.menu > li")];
 const searchWrapEl = headerEl.querySelector(".search-wrap");
 const searchStarterEl = headerEl.querySelector(".search-starter");
 const searchCloserEl = searchWrapEl.querySelector(".search-closer");
 const searchShadowEl = searchWrapEl.querySelector(".shadow");
+const searchDelayEls = [...searchWrapEl.querySelectorAll("li")];
 
 searchStarterEl.addEventListener("click", showSearch);
 searchCloserEl.addEventListener("click", hideSearch);
@@ -42,14 +43,22 @@ searchShadowEl.addEventListener("click", hideSearch);
 function showSearch() {
   headerEl.classList.add("searching");
   document.documentElement.classList.add("fixed");
-  hedaerMenuEls.reverse().forEach(function (el, index) {
-    el.style.transitionDelay = (index * 0.4) / hedaerMenuEls.length + "s";
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + "s";
+  });
+  searchDelayEls.forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / searchDelayEls.length + "s";
   });
 }
 function hideSearch() {
   headerEl.classList.remove("searching");
   document.documentElement.classList.remove("fixed");
-  hedaerMenuEls.reverse().forEach(function (el, index) {
-    el.style.transitionDelay = (index * 0.4) / hedaerMenuEls.length + "s";
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + "s";
   });
+  searchDelayEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / searchDelayEls.length + "s";
+  });
+  //hideSearch에서 reverse 된 채로 끝나면 showSearch에서 revese된 채로 다시 실행되니 ' searchDelayEls.reverse()'를 추가해야함!
+  searchDelayEls.reverse();
 }
