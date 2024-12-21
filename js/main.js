d57dc4c9794be84bb1adf83f13ef4889193c92fa
gayoung lee <ken604@naver.com>
2024-12-22 02:03:51 +0900

    비디오 추가 및 재생, 일시정지 자바스크립트 실행 완료

--- js/main.js
+++ js/main.js
@@ -70,7 +70,6 @@ function hideSearch() {
 }
 
 // 요소가 가시성 관찰!
-
 const io = new IntersectionObserver(function (entries) {
   entries.forEach(function (entry) {
     if (!entry.isIntersecting) {
@@ -83,4 +82,21 @@ const io = new IntersectionObserver(function (entries) {
 const infoEls = document.querySelectorAll(".info");
 infoEls.forEach(function (el) {
   io.observe(el);
+});
+
+// 동영상 재생 및 일시정지
+const video = document.querySelector(".stage video");
+const playBtn = document.querySelector(".stage .controller--play");
+const pauseBtn = document.querySelector(".stage .controller--pause");
+
+playBtn.addEventListener("click", function () {
+  video.play();
+  playBtn.classList.add("hide");
+  pauseBtn.classList.remove("hide");
+});
+
+pauseBtn.addEventListener("click", function () {
+  video.pause();
+  playBtn.classList.remove("hide");
+  pauseBtn.classList.add("hide");
 });
